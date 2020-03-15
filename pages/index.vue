ddfb21688cc4b246dc8e765a1102bcffc717aff0
Recito Gigih P <recitogigih9@gmail.com>
2020-03-15 16:31:14 +0700

    done with bug

--- pages/index.vue
+++ pages/index.vue
@@ -341,10 +341,22 @@ box-shadow: 10px 10px 39px -6px rgba(0,0,0,0.75);">
         recoveredByCountries: '',
         deathByCountries: '',
 
+
         arrConfirmPercentage: [],
         zz: [],
         arrRecoveredPercentage: [],
-        arrDeathPercentage: []
+        arrDeathPercentage: [],
+
+        hasilDataMap: [],
+     
+        dataMaps: '',
+
+        title: '',
+        lat: '',
+        long: '',
+        xx: [],
+        dataobj:''
+
 
 
 
@@ -357,8 +369,9 @@ box-shadow: 10px 10px 39px -6px rgba(0,0,0,0.75);">
       this.getConfirmbyCountry();
       this.getRecoveredbyCountry();
       this.getDeathbyCountry();
+       this.dataForChart()
       this.barchart();
-
+     
     },
 
 
@@ -400,9 +413,13 @@ box-shadow: 10px 10px 39px -6px rgba(0,0,0,0.75);">
         this.$axios.get('https://covid19.mathdro.id/api/confirmed')
           .then(response => {
             this.confirmByCountries = response.data
+            // console.log('OOSMDDN' + this.confirmByCountries)
+            // console.log('dasdad' + this.confirmByCountries.length)
             let i = 0
             for (i; i < this.confirmByCountries.length; i++) {
+
               let z = this.confirmByCountries[i].confirmed
+
 
               let arrzz = Number(z).toLocaleString()
               this.zz.push(arrzz)
@@ -473,6 +490,60 @@ box-shadow: 10px 10px 39px -6px rgba(0,0,0,0.75);">
           currentdate.getSeconds();
       },
 
+
+      dataForChart() {
+
+        //      let {
+        //   am4core,
+        //   am4charts,
+        //   am4maps,
+        //   am4themes_animated,
+        //   am4geodata_worldLow,
+        //   am4themes_dark,
+
+        // } = this.$am4core();
+       
+        this.$axios.get('https://covid19.mathdro.id/api/confirmed')
+          .then(response => {
+            this.dataMaps = response.data
+            // this.databgst = this.dataMaps[i]
+            // this.lat = this.dataMaps[i].lat;
+            // this.long = this.this.dataMaps[i].long;
+            // console.log('data map-> ' + this.dataMaps)
+
+            //  let colorSet = new am4core.ColorSet();
+
+            for (let i = 0; i < this.dataMaps.length; i++) {
+              // console.log( this.dataMaps.length)
+              // console.log([i])
+              // console.log('sadadadaadadsd' + this.dataMaps[i].long)
+
+               this.dataobj = {
+                "title": this.dataMaps[i].countryRegion +' ' + this.dataMaps[i].provinceState,
+                "latitude": this.dataMaps[i].lat,
+                "longitude": this.dataMaps[i].long,
+                // "color": colorSet.next(),
+              }
+               this.xx.push(this.dataobj)
+              // this.res.push(dataobj)
+              //  this.hasilDataMap.push(this.xx)
+
+              console.log(this.xx)
+
+              //  return this.xx
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          });
+
+
+      },
+
+     
+
+
+
       barchart() {
         let {
           am4core,
@@ -525,7 +596,6 @@ box-shadow: 10px 10px 39px -6px rgba(0,0,0,0.75);">
         circle2.radius = 3;
         circle2.propertyFields.fill = "color";
 
-
         circle2.events.on("inited", function (event) {
           animateBullet(event.target);
         })
@@ -548,114 +618,113 @@ box-shadow: 10px 10px 39px -6px rgba(0,0,0,0.75);">
 
         let colorSet = new am4core.ColorSet();
 
-        imageSeries.data = [{
-          "title": "Brussels",
-          "latitude": 50.8371,
-          "longitude": 4.3676,
-          "color": colorSet.next()
-        }, {
-          "title": "Copenhagen",
-          "latitude": 55.6763,
-          "longitude": 12.5681,
-          "color": colorSet.next()
-        }, {
-          "title": "Paris",
-          "latitude": 48.8567,
-          "longitude": 2.3510,
-          "color": colorSet.next()
-        }, {
-          "title": "Reykjavik",
-          "latitude": 64.1353,
-          "longitude": -21.8952,
-          "color": colorSet.next()
-        }, {
-          "title": "Moscow",
-          "latitude": 55.7558,
-          "longitude": 37.6176,
-          "color": colorSet.next()
-        }, {
-          "title": "Madrid",
-          "latitude": 40.4167,
-          "longitude": -3.7033,
-          "color": colorSet.next()
-        }, {
-          "title": "London",
-          "latitude": 51.5002,
-          "longitude": -0.1262,
-          "url": "http://www.google.co.uk",
-          "color": colorSet.next()
-        }, {
-          "title": "Peking",
-          "latitude": 39.9056,
-          "longitude": 116.3958,
-          "color": colorSet.next()
-        }, {
-          "title": "New Delhi",
-          "latitude": 28.6353,
-          "longitude": 77.2250,
-          "color": colorSet.next()
-        }, {
-          "title": "Tokyo",
-          "latitude": 35.6785,
-          "longitude": 139.6823,
-          "url": "http://www.google.co.jp",
-          "color": colorSet.next()
-        }, {
-          "title": "Ankara",
-          "latitude": 39.9439,
-          "longitude": 32.8560,
-          "color": colorSet.next()
-        }, {
-          "title": "Buenos Aires",
-          "latitude": -34.6118,
-          "longitude": -58.4173,
-          "color": colorSet.next()
-        }, {
-          "title": "Brasilia",
-          "latitude": -15.7801,
-          "longitude": -47.9292,
-          "color": colorSet.next()
-        }, {
-          "title": "Ottawa",
-          "latitude": 45.4235,
-          "longitude": -75.6979,
-          "color": colorSet.next()
-        }, {
-          "title": "Washington",
-          "latitude": 38.8921,
-          "longitude": -77.0241,
-          "color": colorSet.next()
-        }, {
-          "title": "Kinshasa",
-          "latitude": -4.3369,
-          "longitude": 15.3271,
-          "color": colorSet.next()
-        }, {
-          "title": "Cairo",
-          "latitude": 30.0571,
-          "longitude": 31.2272,
-          "color": colorSet.next()
-        }, {
-          "title": "Pretoria",
-          "latitude": -25.7463,
-          "longitude": 28.1876,
-          "color": colorSet.next()
-        }];
+    
 
+        imageSeries.data =  this.xx
 
+        // imageSeries.data = [{
+        //   "title": "Brussels",
+        //   "latitude": 50.8371,
+        //   "longitude": 4.3676,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Copenhagen",
+        //   "latitude": 55.6763,
+        //   "longitude": 12.5681,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Paris",
+        //   "latitude": 48.8567,
+        //   "longitude": 2.3510,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Reykjavik",
+        //   "latitude": 64.1353,
+        //   "longitude": -21.8952,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Moscow",
+        //   "latitude": 55.7558,
+        //   "longitude": 37.6176,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Madrid",
+        //   "latitude": 40.4167,
+        //   "longitude": -3.7033,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "London",
+        //   "latitude": 51.5002,
+        //   "longitude": -0.1262,
+        //   "url": "http://www.google.co.uk",
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Peking",
+        //   "latitude": 39.9056,
+        //   "longitude": 116.3958,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "New Delhi",
+        //   "latitude": 28.6353,
+        //   "longitude": 77.2250,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Tokyo",
+        //   "latitude": 35.6785,
+        //   "longitude": 139.6823,
+        //   "url": "http://www.google.co.jp",
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Ankara",
+        //   "latitude": 39.9439,
+        //   "longitude": 32.8560,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Buenos Aires",
+        //   "latitude": -34.6118,
+        //   "longitude": -58.4173,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Brasilia",
+        //   "latitude": -15.7801,
+        //   "longitude": -47.9292,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Ottawa",
+        //   "latitude": 45.4235,
+        //   "longitude": -75.6979,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Washington",
+        //   "latitude": 38.8921,
+        //   "longitude": -77.0241,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Kinshasa",
+        //   "latitude": -4.3369,
+        //   "longitude": 15.3271,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Cairo",
+        //   "latitude": 30.0571,
+        //   "longitude": 31.2272,
+        //   "color": colorSet.next()
+        // }, {
+        //   "title": "Pretoria",
+        //   "latitude": -25.7463,
+        //   "longitude": 28.1876,
+        //   "color": colorSet.next()
+        // }];
 
       },
 
-      // beforeDestroy() {
-      //   if (this.chart) {
-      //     this.chart.dispose();
-      //   }
-      // },
+      beforeDestroy() {
 
-
-
-
-
+        
+        if (this.chart) {
+          this.chart.dispose();
+        }
+      },
     }
   }
 
